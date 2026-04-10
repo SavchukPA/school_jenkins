690b6abcd852e42067b0455f4641de3e6743bb0f
Sergei Krivonosov <ksva690@gmail.com>
2026-04-10 22:19:47 +0200

    after review

--- tests/test_group_costa_blanca_qa.py
+++ tests/test_group_costa_blanca_qa.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+import time
 
 
 def test_storyia_submit_form(browser):
@@ -22,22 +23,21 @@ def test_storyia_checkbox_selected(browser):
 
     assert checkbox.is_selected()
 
-    #########################################################################
 
-    from selenium.webdriver.common.by import By
-    import time
 
-    def test_tittle_sergio_qa(browser):
-        '''Тест проверки заголовка страницы'''
-        browser.get('https://martspec.com/')
-        assert 'Simplify health & wellness tracking' in browser.title
 
-    def test_change_language_sergio_qa(browser):
-        '''Раскрыть список языков веб-сайта'''
-        browser.get('https://martspec.com/')
 
-        dropdown = browser.find_element(By.ID, 'navbarDropdown')
-        dropdown.click()
+def test_title_sergio_qa(browser):
+    '''Тест проверки заголовка страницы'''
+    browser.get('https://martspec.com/')
+    assert 'Simplify health & wellness tracking' in browser.title
 
-        time.sleep(3)
+def test_change_language_sergio_qa(browser):
+    '''Раскрыть список языков веб-сайта'''
+    browser.get('https://martspec.com/')
+
+    dropdown = browser.find_element(By.ID, 'navbarDropdown')
+    dropdown.click()
+
+    time.sleep(3)
 
