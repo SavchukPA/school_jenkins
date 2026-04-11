81d42b7ca7d7234f0add86ee5064f1ae77ac3207
Yuliya Bozheva <yulka818818@gmail.com>
2026-04-11 13:45:07 -0400

    add first test accordion

--- tests/test_accordion_yul_b.py
+++ tests/test_accordion_yul_b.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+
 def test_accordion_yul_b(browser):
     browser.get("https://www.automationtesting.co.uk/index.html")
     browser.maximize_window()
