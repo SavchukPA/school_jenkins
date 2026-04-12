3d7dfd6e9984eefdbd30e93d40d7a4e0213e8f44
tanyaIlina4199 <132301479+tanyaIlina4199@users.noreply.github.com>
2026-04-13 00:13:18 +0200

    Fix Power Tools checkbox (#75)

--- tests/test_toolshop_filters.py
+++ tests/test_toolshop_filters.py
@@ -1,15 +1,27 @@
 import time
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_toolshop_filters(browser):
     browser.get("https://practicesoftwaretesting.com/")
-    time.sleep(1)
-    power_tools_checkbox = browser.find_element(By.CSS_SELECTOR, '[data-test="category-01KNY366MVN0YAXXH9MC8HC1RB"]')
+    wait = WebDriverWait(browser, 10)
+
+    power_tools_checkbox = wait.until(
+        EC.element_to_be_clickable((
+            By.XPATH,
+            '//label[contains(normalize-space(.), "Power Tools")]//input[@type="checkbox"]'
+        ))
+    )
 
     power_tools_checkbox.click()
 
-    time.sleep(1)
-    sheet_sander_product = browser.find_element(By.CSS_SELECTOR, '[data-test="product-name"]')
+    sheet_sander_product = wait.until(
+        EC.visibility_of_element_located((
+            By.XPATH,
+            '//h5[@data-test="product-name" and normalize-space()="Sheet Sander"]'
+        ))
+    )
     assert sheet_sander_product.text == "Sheet Sander"
