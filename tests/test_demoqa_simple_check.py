d6d3dee7bbb8a0ce61f06680ebc55c287f250c03
ZAVGOR-NOTE1\M.Zavgorodneva <mariya.zavgorodneva@onetwotrip.com>
2026-04-12 11:50:40 +0300

    test widgets text

--- tests/test_demoqa_simple_check.py
+++ tests/test_demoqa_simple_check.py
@@ -1,17 +1,3 @@
-# from selenium.webdriver.common.by import By
-#
-# def test_check_widgets_text(browser):
-#     browser.get("https://demoqa.com/")
-#
-#     # browser.find_element(By.XPATH, "//*[@id='root']/div/div/div[2]/div/a[3]").click()
-#     # browser.find_element(By.XPATH, "//span[text()='Alerts']").click()
-#     # browser.find_element(By.ID, "confirmButton").click()
-#     #
-#     # alert = browser.switch_to.alert
-#     # alert.accept()
-#     #
-#     # assert browser.find_element(By.ID, "confirmResult").text == "You selected Ok"
-
 from selenium.webdriver.common.by import By
 
 
