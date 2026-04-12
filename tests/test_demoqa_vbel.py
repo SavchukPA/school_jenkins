f470a0fab1d0dfa6eb465c0fa14d56df0bf14a9f
Vyacheslav Belyaev <vbelyaev86@gmail.com>
2026-04-12 23:52:22 +0300

    add second test: check swirch to new tab

--- tests/test_demoqa_vbel.py
+++ tests/test_demoqa_vbel.py
@@ -1,8 +1,11 @@
+import time
 
 from selenium.webdriver.common.by import By
 
+demoqa_url = "https://demoqa.com/"
+
 def test_check_open_alert(browser):
-    browser.get("https://demoqa.com/")
+    browser.get(demoqa_url)
 
     browser.find_element(By.XPATH, "//*[@id='root']/div/div/div[2]/div/a[3]").click()
     browser.find_element(By.XPATH, "//span[text()='Alerts']").click()
@@ -13,5 +16,15 @@ def test_check_open_alert(browser):
 
     assert browser.find_element(By.ID, "confirmResult").text == "You selected Ok"
 
+def test_check_switch_new_tab(browser):
+    browser.get(demoqa_url)
 
+    browser.find_element(By.XPATH, "//*[@id='root']/div/div/div[2]/div/a[3]").click()
+    browser.find_element(By.XPATH, "//span[text()='Browser Windows']").click()
+    browser.find_element(By.ID, "tabButton").click()
+
+    window_handles = browser.window_handles
+    browser.switch_to.window(window_handles[1])
+
+    assert browser.find_element(By.ID, "sampleHeading").text == "This is a sample page"
 
