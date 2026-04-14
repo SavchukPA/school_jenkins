04030f1e71a022237bc0f2fca510a1aaf23ba550
krispavlovicheva-png <krispavlovicheva@gmail.com>
2026-04-14 18:03:05 +0300

    Delete tests/test_ksu.py

--- tests/test_ksu.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get("https://ya.ru")
-
-# Проверка: слово "Яндекс" есть в заголовке страницы
-assert "Яндекс" in driver.title
-
-driver.quit()
