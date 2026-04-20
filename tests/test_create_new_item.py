7cb7d002446984440b51f9feb2c7ab9ee4be5c64
Andrii Koss <sqakross@gmail.com>
2026-04-19 21:12:54 -0700

    TC_01.001.03 update

--- tests/test_create_new_item.py
+++ tests/test_create_new_item.py
@@ -13,16 +13,12 @@ def test_create_new_item_validate_unsupported_special_characters(browser, invali
     name_input.clear()
     name_input.send_keys(f"test{invalid_char}job")
 
-    # wait.until(
-    #     EC.element_to_be_clickable((By.CSS_SELECTOR, "label[for='hudson_model_FreeStyleProject']"))
-    # )
-
     wait.until(
         EC.text_to_be_present_in_element((By.ID, "itemname-invalid"), "unsafe character")
     )
     warning = browser.find_element(By.ID, "itemname-invalid")
+    ok_button = browser.find_element(By.ID, "ok-button")
+
     assert warning.is_displayed()
     assert "unsafe character" in warning.text
-
-    ok_button = browser.find_element(By.ID, "ok-button")
     assert ok_button.get_attribute("disabled") is not None
