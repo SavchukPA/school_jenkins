7daa17145dae2fb00ee67c822c82af02a3e0849d
Sofia Afanaseva <sofiskorikova@mail.ru>
2026-04-20 21:24:37 +0300

    add new test: test_unsupported_characters

---
+++ tests/test_unsupported_characters.py
@@ -0,0 +1,29 @@
+import random
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def random_characters():
+    unsupported_characters = ["?", "*", "/", "|", "!", "%", "&", ";", ":"]
+    return random.choice(unsupported_characters)
+
+def test_unsupported_characters(browser):
+    character = random_characters()
+
+    new_item = browser.find_element(By.XPATH, "//a[@it]")
+    new_item.click()
+
+    new_item_field = browser.find_element(By.XPATH, "//input[@name='name']")
+    new_item_field.send_keys(character)
+
+    wait = WebDriverWait(browser, 2)
+
+    message = wait.until(
+        EC.visibility_of_element_located((By.XPATH, "//div[@id='itemname-invalid']"))).text
+
+    assert message == f"» ‘{character}’ is an unsafe character"
+
+    button = browser.find_element(By.XPATH, "//button[@id='ok-button']")
+
+    assert button.is_enabled() is False
