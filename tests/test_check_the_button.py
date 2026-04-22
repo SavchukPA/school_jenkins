d7e409b2b3998f483160d46346e8d15d51647957
TatianaBurkova <burkovatatiana7@gmail.com>
2026-04-22 21:10:05 +0700

    skip unstable test

--- tests/test_check_the_button.py
+++ tests/test_check_the_button.py
@@ -1,4 +1,5 @@
 import time
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
