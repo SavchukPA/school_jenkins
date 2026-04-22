202163ea581b8bd635c0fdb174375a95aa0f79a7
taras <tarsic99@gmail.com>
2026-04-22 01:08:59 -0400

    Adding more explicit waits.

--- tests/test_copy_from_text_field.py
+++ tests/test_copy_from_text_field.py
@@ -28,15 +28,15 @@ def test_copy_from_text_field(browser):
 
     browser.find_element(By.CSS_SELECTOR, "#jenkins-head-icon").click()
     browser.find_element(By.XPATH, "//a[@href='/view/all/newJob']").click()
-    browser.execute_script("window.scrollBy({ top: 800, left: 0, behavior: 'auto' });")
+    browser.execute_script("window.scrollBy({ top: 900, left: 0, behavior: 'auto' });")
     wait.until(ec.visibility_of_element_located((By.XPATH, "(//label[normalize-space()='Copy from'])[1]")))
 
     text_field_name = browser.find_element(By.XPATH, "(//label[normalize-space()='Copy from'])[1]")
     assert text_field_name.text == "Copy from"
 
     text_field = browser.find_element(By.XPATH, "(//input[@id='from'])[1]")
-    text_field.click()
-    text_field.send_keys("my_first_pipeline")
+    wait.until(ec.element_to_be_clickable((By.XPATH, "(//input[@id='from'])[1]"))).click()
+    wait.until(ec.element_to_be_clickable((By.XPATH, "(//input[@id='from'])[1]"))).send_keys("my_first_pipeline")
 
     typed_text = text_field.get_attribute("value")
     assert typed_text == "my_first_pipeline"
