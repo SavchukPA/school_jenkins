cbcdec1d701df767e8696fc63c359dd82cf8bff0
SvetlanaKazakova <mks.kazakova@mail.ru>
2026-04-23 12:20:07 +0300

    TC_01.001.11 Create_Organization_Folder

--- tests/test_create_new_job.py
+++ tests/test_create_new_job.py
@@ -13,6 +13,5 @@ def test_create_organization_folder(browser):
     browser.find_element(By.CLASS_NAME, "com_cloudbees_hudson_plugins_folder_Folder").click()
     browser.find_element(By.ID, "ok-button").click()
     browser.find_element(By.NAME, "Submit").click()
-
     assert f"/job/{FOLDER_NAME}/" in browser.current_url
     assert browser.find_element(By.CLASS_NAME, "job-index-headline").text == FOLDER_NAME
