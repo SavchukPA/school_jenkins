f3e7a38ed1a1867ab760d3d5824b7f2d2243525c
SvetlanaKazakova <mks.kazakova@mail.ru>
2026-04-23 12:22:51 +0300

    TC_01.001.11 Create_Organization_Folder

--- tests/test_create_new_job.py
+++ tests/test_create_new_job.py
@@ -8,7 +8,6 @@ FOLDER_NAME = "TestOrganizationFolder"
 
 def test_create_organization_folder(browser):
     browser.find_element(By.LINK_TEXT, "New Item").click()
-
     browser.find_element(By.ID, "name").send_keys(FOLDER_NAME)
     browser.find_element(By.CLASS_NAME, "com_cloudbees_hudson_plugins_folder_Folder").click()
     browser.find_element(By.ID, "ok-button").click()
