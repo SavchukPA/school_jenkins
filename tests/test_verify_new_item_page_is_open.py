dd74bd8e2146f18e763775b58ca9e1abd822cc25
Igor <osinus-sb@ya.ru>
2026-04-24 22:58:45 +0700

    Fixed test instability in CI: Add explicit wait

--- tests/test_verify_new_item_page_is_open.py
+++ tests/test_verify_new_item_page_is_open.py
@@ -1,8 +1,14 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_open_new_item_page(browser):
-    browser.get("http://localhost:8080")
+    WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.LINK_TEXT, "New Item"))
+    ).click()
 
-    browser.find_element("link text", "New Item").click()
+    WebDriverWait(browser, 10).until(
+        EC.url_contains("/newJob")
+    )
 
     assert "/newJob" in browser.current_url
