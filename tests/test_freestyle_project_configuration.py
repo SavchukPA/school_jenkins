fbe66abf13c7bd788dcf66f08fb94f785c39769f
TatianaBurkova <burkovatatiana7@gmail.com>
2026-04-26 23:16:43 +0700

    refactor test

---
+++ tests/test_freestyle_project_configuration.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.common.by import By
+
+
+def create_freestyle_project(driver):
+
+    driver.find_element(By.XPATH, "//a[@href='/view/all/newJob']").click()
+    driver.find_element(By.ID, "name").send_keys('Test')
+    driver.find_element(By.CSS_SELECTOR, "li.hudson_model_FreeStyleProject").click()
+    driver.find_element(By.XPATH, '//*[@id="ok-button"]').click()
+    driver.find_element(By.NAME, "Submit").click()
+
+    assert driver.find_element(By.XPATH, "//*[contains(text(), 'Test')]")
+
+
+def test_disable_active_project(browser):
+    create_freestyle_project(browser)
+
+    browser.find_element(By.ID, 'jenkins-head-icon').click()
+
+    browser.find_element(By.CSS_SELECTOR, 'button.jenkins-menu-dropdown-chevron').click()
+    browser.find_element(By.XPATH, "//a[contains(@href, 'configure')]").click()
+
+    browser.find_element(By.CSS_SELECTOR, 'label[for="enable-disable-project"]').click()
+    browser.find_element(By.NAME, "Submit").click()
+
+    assert browser.find_element(
+        By.XPATH, "//*[contains(text(), 'This project is currently disabled')]"
+    )
