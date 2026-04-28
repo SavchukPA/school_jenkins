4e3dd09120fa2d0387de2c67e35494b54a702caf
Sergey <space-researcher@i.ua>
2026-04-28 21:17:47 +0300

    TC_09.001.01 | Build history - text

--- tests/test_build_history.py
+++ tests/test_build_history.py
@@ -2,6 +2,6 @@ from selenium.webdriver.common.by import By
 
 def test_build_history(browser):
     browser.find_element(By.CSS_SELECTOR, "a[href='/view/all/builds']").click()
-    label = browser.find_element(By.CSS_SELECTOR, "div.jenkins-app-bar__content > h1")
+    label = browser.find_element(By.CSS_SELECTOR, "div.jenkins-app-bar__content > h1").text
 
-    assert label.text == "Build History of Jenkins"
+    assert label == "Build History of Jenkins"
