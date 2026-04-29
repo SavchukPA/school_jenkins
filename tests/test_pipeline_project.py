49df93f061cd4c8e39597373d9160525382f11f7
Vyacheslav Belyaev <vbelyaev86@gmail.com>
2026-04-29 23:49:27 +0300

    RF_01.001.02 || Edit function create job and search locators

--- tests/test_pipeline_project.py
+++ tests/test_pipeline_project.py
@@ -10,15 +10,17 @@ def create_pipeline_project(browser, name):
     browser.find_element(By.XPATH, "//a[@href='/view/all/newJob']").click()
     browser.find_element(By.ID, "name").send_keys(name)
     browser.find_element(By.CLASS_NAME, "org_jenkinsci_plugins_workflow_job_WorkflowJob").click()
-
     browser.find_element(By.ID, "ok-button").click()
 
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
     WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.NAME, "Submit"))).click()
 
-    WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.XPATH, "//*[@class='app-jenkins-logo']"))).click()
+    browser.execute_script("""
+                var logo = document.querySelector('.jenkins-mobile-hide');
+                if (logo) logo.click();
+            """)
 
-@pytest.mark.skip
 def test_create_project(browser):
     create_pipeline_project(browser, pipeline_name)
     created_pipeline = browser.find_element(By.XPATH, f"(//a[@href='job/{pipeline_name}/'])[1]").text
@@ -31,7 +33,6 @@ def test_create_project(browser):
     "PipelineName3"
 ])
 
-@pytest.mark.skip
 def test_check_several_tests(browser, job_name):
     create_pipeline_project(browser, job_name)
     created_pipeline = browser.find_element(By.CSS_SELECTOR, ".jenkins-table__link >span:first-child").text
