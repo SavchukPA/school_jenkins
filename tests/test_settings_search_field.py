dda6e3a0cc72b155cfdf507e04cfcec3137a2177
zevs-wot <zevs.wot.zevs@mail.ru>
2026-04-29 22:45:10 +0400

    Changed the locator - CSS to XPATH, because testing is interrupted in CI.

--- tests/test_settings_search_field.py
+++ tests/test_settings_search_field.py
@@ -10,9 +10,9 @@ def test_checking_the_dropdown_when_entering_one_letter(browser):
     browser.find_element(By.ID, 'root-action-ManageJenkinsAction').click()
 
     _get_field_search(browser).send_keys('t')
-    items = browser.find_elements(By.CSS_SELECTOR, '.jenkins-search__results-container--visible a')
+    items = browser.find_elements(
+        By.XPATH,
+        '//*[contains(@class, "jenkins-search__results-container--visible")]//a[contains(translate(., "T", "t"), "t")]'
+    )
 
     assert len(items) > 1
-
-    for item in items:
-        assert 't' in item.text.lower(), f"Элемент {item.text} не содержит 't'"
