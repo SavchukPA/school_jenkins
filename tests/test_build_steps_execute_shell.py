533d509b546647fd0749c4192f13bd70a3a59324
Sergey <space-researcher@i.ua>
2026-04-29 15:38:32 +0300

    TC_09.001.01 | Build history - text

--- tests/test_build_steps_execute_shell.py
+++ tests/test_build_steps_execute_shell.py
@@ -3,9 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
-def test_create_and_run_freestyle_job(browser):
-    """Тест создаёт New Item (новую джобу) Freestyle project,
-    добавляет простой bash-скрипт, запускает в Дженкинсе и проверяет логи"""
+def test_build_steps_execute_shell(browser):
 
     script_for_linux = '''echo "Starting process..."
     echo "Hostname: $(hostname)"
@@ -14,10 +12,10 @@ def test_create_and_run_freestyle_job(browser):
     sleep 1
     echo "1 seconds have passed. Continuing..." '''
 
-    browser.find_element(By.XPATH, '//*[@id="tasks"]/div[1]/span/a').click()
+    browser.find_element(By.XPATH, '//*[@id="tasks"]//a').click()
     input_name = browser.find_element(By.ID, "name")
     input_name.send_keys("My_test")
-    browser.find_element(By.XPATH, '//*[@id="j-add-item-type-standalone-projects"]/ul/li[2]').click()
+    browser.find_element(By.CLASS_NAME, 'hudson_model_FreeStyleProject').click()
     browser.find_element(By.XPATH, '//*[@id="ok-button"]').click()
 
     add_button = WebDriverWait(browser, 10).until(
@@ -27,28 +25,14 @@ def test_create_and_run_freestyle_job(browser):
     browser.execute_script("arguments[0].scrollIntoView({block: 'center'});", add_button)
     add_button.click()
 
-    WebDriverWait(browser, 5).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, "//button[normalize-space()='Execute shell']")
-        )
-    ).click()
+    browser.find_element(By.XPATH, "//button[normalize-space()='Execute shell']").click()
 
     editor = browser.find_element(By.CSS_SELECTOR, ".CodeMirror")
     ActionChains(browser).move_to_element(editor).click().send_keys(script_for_linux).perform()
 
     browser.find_element(By.XPATH, '//*[@id="bottom-sticker"]/div/button[1]').click()
-
-    WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, "//a[.//span[normalize-space()='Build Now']]")
-        )
-    ).click()
-
-    WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, '//a[@href="/job/My_test/1/console"]')
-        )
-    ).click()
+    browser.find_element(By.XPATH, "//a[.//span[normalize-space()='Build Now']]").click()
+    browser.find_element(By.XPATH, '//a[@href="/job/My_test/1/console"]').click()
 
     log = browser.find_element(By.ID, "out").text
 
