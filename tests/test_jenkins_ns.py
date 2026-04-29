b5c55a1a5d2a23e1d32461c8324b5edca25827e7
NataliiaSerkes <serkes.natasha@gmail.com>
2026-04-29 05:54:14 +0200

    Edd new test ns

--- tests/test_jenkins_ns.py
+++ tests/test_jenkins_ns.py
@@ -2,14 +2,18 @@ import time
 from selenium.webdriver.common.by import By
 
 def test_jenkins_ns(browser):
-    browser.find_element(By.XPATH, "//*[@id='tasks']/div[1]/span/a").click()
+    browser.find_element(By.LINK_TEXT, "New Item").click()
 
     browser.find_element(By.ID, "name").send_keys("test_1")
-    browser.find_element(By.XPATH,"//*[@id='j-add-item-type-standalone-projects']/ul/li[1]").click()
+    browser.find_element(By.XPATH,"//ul/li//span[text()='Pipeline']").click()
     browser.find_element(By.ID, "ok-button").click()
     time.sleep(6)
-    browser.find_element(By.XPATH, "//*[@id='bottom-sticker']/div/button[1]").click()
-    time.sleep(5)
-    label=browser.find_element(By.XPATH, "//*[@id='main-panel']/div[1]/div[1]/h1")
 
+    browser.find_element(By.NAME,"Submit").click()
+    time.sleep(5)
+
+    browser.find_element(By.XPATH, "//*[@class='app-jenkins-logo']").click()
+    time.sleep(2)
+
+    label=browser.find_element(By.XPATH, "//*[@href='job/test_1/']")
     assert label.text == "test_1"
