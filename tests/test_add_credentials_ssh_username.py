448c6e3aec120304d806535ba62f99dd3e236f10
muravlev.dv <barcenal1985@gmail.com>
2026-05-02 19:00:03 +0300

    change selector //a[@href='/manage/credentials/store/system/domain/_/']

--- tests/test_add_credentials_ssh_username.py
+++ tests/test_add_credentials_ssh_username.py
@@ -6,7 +6,10 @@ from selenium.webdriver.support.ui import Select
 def test_add_credentials_ssh_username(browser):
     browser.find_element(By.ID, 'root-action-ManageJenkinsAction').click()
     browser.find_element(By.XPATH, "//*[@href='credentials']").click()
-    browser.find_element(By.XPATH, "//a[normalize-space()='(global)']").click()
+    wait = WebDriverWait(browser, 10)
+    wait.until(EC.element_to_be_clickable((
+        By.XPATH, "//a[@href='/manage/credentials/store/system/domain/_/']"
+    ))).click()
     wait = WebDriverWait(browser, 10)
     wait.until(EC.visibility_of_element_located((By.XPATH, "(//a[normalize-space()='Add credentials'])[1]"))).click()
 
