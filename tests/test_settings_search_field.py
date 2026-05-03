b72468573882217856bda488f6b1b02e6a3ae6e4
zevs-wot <zevs.wot.zevs@mail.ru>
2026-05-03 21:31:16 +0400

    1. removed the locator functions and wrote them into each test 2. add WebDriverWait into every test 3. repair test number 2

--- tests/test_settings_search_field.py
+++ tests/test_settings_search_field.py
@@ -1,20 +1,17 @@
 from selenium.webdriver import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from conftest import browser
-
-
-def _get_field_search(driver):
-    return driver.find_element(By.ID, 'settings-search-bar')
-
-
-def _get_manage_jenkins_btn(driver):
-    return driver.find_element(By.ID, 'root-action-ManageJenkinsAction')
 
 
 def test_checking_the_dropdown_when_entering_one_letter(browser):
-    _get_manage_jenkins_btn(browser).click()
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.ID, 'root-action-ManageJenkinsAction'))
+    ).click()
 
-    _get_field_search(browser).send_keys('t')
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.ID, 'settings-search-bar'))
+    ).send_keys('t')
     items = browser.find_elements(
         By.XPATH,
         '//*[contains(@class, "jenkins-search__results-container--visible")]//a[contains(translate(., "T", "t"), "t")]'
@@ -24,20 +21,29 @@ def test_checking_the_dropdown_when_entering_one_letter(browser):
 
 
 def test_checking_the_dropdown_when_entering_full_name_settings(browser):
-    _get_manage_jenkins_btn(browser).click()
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.ID, 'root-action-ManageJenkinsAction'))
+    ).click()
 
-    _get_field_search(browser).send_keys('Security')
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.ID, 'settings-search-bar'))
+    ).send_keys('Security')
     item = browser.find_element(
         By.XPATH,
         '//*[contains(@class,"jenkins-search__results-container--visible")]//a[normalize-space()="Security"]'
     )
 
-    assert item.text.strip() == 'Security'
+    assert item.get_attribute('textContent').strip() == 'Security'
 
 
 def test_clear_search_field_via_keyboard_and_button(browser):
-    _get_manage_jenkins_btn(browser).click()
-    field = _get_field_search(browser)
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.ID, 'root-action-ManageJenkinsAction'))
+    ).click()
+
+    field = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.ID, 'settings-search-bar'))
+    )
 
     field.send_keys('System')
     field.send_keys(Keys.CONTROL + 'a')
