477639c49e96b434753ece9368d3ea4f8331cb2e
muravlev.dv <barcenal1985@gmail.com>
2026-05-03 14:20:52 +0300

    add test_add_credentials_ssh_username.py

--- tests/test_add_credentials_ssh_username.py
+++ tests/test_add_credentials_ssh_username.py
@@ -1,38 +1,25 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
+test_id = "testID"
 
 def test_add_credentials_ssh_username(browser):
+
     browser.find_element(By.ID, 'root-action-ManageJenkinsAction').click()
-    browser.find_element(By.XPATH, "//*[@href='credentials']").click()
-    wait = WebDriverWait(browser, 30)
-    wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@href='/manage/credentials/store/system/domain/_/']"))).click()
-    wait.until(EC.element_to_be_clickable((By.XPATH, "(//a[normalize-space()='Add credentials'])[1]"))).click()
+    browser.find_element(By.CSS_SELECTOR, "a[href='credentials']").click()
 
-    test_id = "testID"
-    description = "testDescription"
-    kind = "SSH Username with private key"
+    browser.find_element(By.CSS_SELECTOR, "button[data-type='credentials-add-store-item']").click()
+    browser.find_element(By.XPATH, "//div[contains(text(),'SSH Username with private key')]").click()
+    browser.find_element(By.ID, 'cr-dialog-next').click()
 
-    select_scope = Select(browser.find_element(By.XPATH,"(//select[@name='_.scope'])[1]"))
-    select_scope.select_by_value('SYSTEM')
-
-    select_kind = Select(browser.find_element(By.CSS_SELECTOR, ".jenkins-select__input"))
-    select_kind.select_by_value('2')
-
-    browser.find_element(By.XPATH, "//*[@checkurl='/manage/descriptorByName/com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey/checkId']").send_keys(test_id)
-    browser.find_element(By.XPATH, "(//input[@name='_.description'])[2]").send_keys(description)
-    browser.find_element(By.XPATH, "(//input[@name='_.username'])[2]").send_keys("testUsername")
-    browser.find_element(By.XPATH, "(//label[contains(@class,'attach-previous') and contains(text(),'Treat username as secret')])[2]").click()
+    Select(browser.find_element(By.NAME,'_.scope')).select_by_value('SYSTEM')
+    browser.find_element(By.NAME,'_.id').send_keys(test_id)
+    browser.find_element(By.NAME,'_.description').send_keys("testDescription")
+    browser.find_element(By.NAME,'_.username').send_keys("testUsername")
+    browser.find_element(By.XPATH, "//label[normalize-space()='Treat username as secret']").click()
     browser.find_element(By.XPATH, "//label[normalize-space()='Enter directly']").click()
     browser.find_element(By.XPATH, "//button[normalize-space()='Add']").click()
-    browser.find_element(By.XPATH, "//textarea").send_keys("testKey")
-    browser.find_element(By.XPATH, "// input[ @ name = '_.passphrase']").send_keys("testPassphrase")
-    browser.find_element(By.XPATH, "//button[normalize-space()='Create']").click()
+    browser.find_element(By.NAME, '_.privateKey').send_keys("testKey")
+    browser.find_element(By.NAME, '_.passphrase').send_keys("testPassphrase")
+    browser.find_element(By.ID, 'cr-dialog-submit').click()
 
-    assert (
-            browser.find_element(By.XPATH, "//a[normalize-space()='testID']").text == test_id and
-            browser.find_element(By.XPATH, "(//td[contains(text(), 'testDescription')])[1]").text == description and
-            browser.find_element(By.XPATH, "(//td[contains(text(), 'SSH Username with private key')])").text == kind and
-            browser.find_element(By.XPATH, "(//td[contains(text(), 'testDescription')])[2]").text == description
-    )
+    assert browser.find_element(By.XPATH, "//a[normalize-space()='testID']").text == test_id
