7ad9fd7784d132f2c2b402ca73e4a8faf7a7e03e
Vyacheslav Belyaev <vbelyaev86@gmail.com>
2026-05-07 23:28:48 +0300

    AT_12.001.01 | Dashboard Verify that can search and open created Item upd3

--- tests/test_multi_configuration_project.py
+++ tests/test_multi_configuration_project.py
@@ -7,7 +7,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 multiconfiguration_project_name = "MultiConfigName"
 
-@pytest.mark.skip(reason="ER_01.001.19")
 def create_multi_configuration_project(browser, name):
     browser.find_element(By.XPATH, "//a[@href='/view/all/newJob']").click()
 
@@ -16,7 +15,7 @@ def create_multi_configuration_project(browser, name):
     browser.find_element(By.ID, "ok-button").click()
 
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.NAME, "Submit"))).click()
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.NAME, "Submit"))).click()
 
     browser.execute_script("""
                     var logo = document.querySelector('.jenkins-mobile-hide');
@@ -31,7 +30,7 @@ def test_verify_status_switching_enable_button(browser):
     browser.find_element(By.CSS_SELECTOR, "#toggle-switch-enable-disable-project > label").click()
     browser.find_element(By.NAME, "Submit").click()
 
-    actual_disable_text = WebDriverWait(browser, 5).until(
+    actual_disable_text = WebDriverWait(browser, 10).until(
         EC.visibility_of_element_located((By.ID, "enable-project"))).text
 
     assert "This project is currently disabled" in actual_disable_text
@@ -46,7 +45,7 @@ def test_verify_enable_toggle_has_tooltip(browser):
     actions = ActionChains(browser)
     actions.move_to_element(enabled_toogle).perform()
 
-    toggle_tooltip = WebDriverWait(browser, 5).until(
+    toggle_tooltip = WebDriverWait(browser, 10).until(
         EC.visibility_of_element_located((By.CLASS_NAME, "tippy-content"))).text
 
     assert toggle_tooltip == "Enable or disable the current project"
@@ -69,7 +68,6 @@ def test_create_item_with_special_characters(browser, special_characters):
     browser.find_element(By.ID, "ok-button").click()
     assert browser.find_element(By.TAG_NAME, "p").text == expected_error_message
 
-@pytest.mark.skip(reason="ER_01.001.19")
 @pytest.mark.dependency()
 def test_create_multi_configuration_project(browser):
     create_multi_configuration_project(browser, multiconfiguration_project_name)
@@ -78,7 +76,6 @@ def test_create_multi_configuration_project(browser):
 
     assert created_multi_configuration == multiconfiguration_project_name
 
-@pytest.mark.skip(reason="ER_01.001.19")
 @pytest.mark.dependency(depends=["test_create_multi_configuration_project"])
 def test_create_project_with_exist_name(browser):
     browser.find_element(By.XPATH, "//a[contains(@href, '/newJob')]").click()
