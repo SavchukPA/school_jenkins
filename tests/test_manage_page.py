7588c498081329fa29471d27bb6118efde4879a7
Petr P <none@none.none>
2026-05-10 22:36:56 +0800

    move function up

--- tests/test_manage_page.py
+++ tests/test_manage_page.py
@@ -5,6 +5,31 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
 
+def create_freestyle_project_with_timer(browser, name_project: str):
+    wait = WebDriverWait(browser, 5)
+
+    wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//a[@href='/view/all/newJob']"))).click()
+    browser.find_element(By.ID, "name").send_keys(name_project)
+    wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//span[text()='Freestyle project']"))).click()
+    browser.find_element(By.XPATH, '//*[@id="ok-button"]').click()
+
+    button_add_build_step = browser.find_element(By.XPATH, "//button[text()='Add build step']")
+    browser.execute_script("arguments[0].scrollIntoView(true);", button_add_build_step)
+    button_add_build_step.click()
+
+    wait.until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "jenkins-dropdown__item")))
+    browser.find_element(By.XPATH, "//button[normalize-space()='Execute shell']").click()
+
+    ActionChains(browser)\
+        .move_to_element(browser.find_element(By.XPATH, "//div[contains(@class, 'cm-s-default')]"))\
+        .click().send_keys("sleep 60").perform()
+
+    browser.find_element(By.NAME, "Submit").click()
+    wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//*[text()='Permalinks']")))
+    browser.find_element(By.CLASS_NAME, "app-jenkins-logo").click()
+
+
+
 def test_verify_navigation_to_manage_page(browser):
     wait = WebDriverWait(browser, 10)
 
@@ -58,27 +83,3 @@ def test_build_queue_visibility(browser):
     target_item = [f"{item_name}{i}" for i in count_jobs]
 
     assert all(item in list_items for item in target_item)
-
-
-def create_freestyle_project_with_timer(browser, name_project: str):
-    wait = WebDriverWait(browser, 5)
-
-    wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//a[@href='/view/all/newJob']"))).click()
-    browser.find_element(By.ID, "name").send_keys(name_project)
-    wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//span[text()='Freestyle project']"))).click()
-    browser.find_element(By.XPATH, '//*[@id="ok-button"]').click()
-
-    button_add_build_step = browser.find_element(By.XPATH, "//button[text()='Add build step']")
-    browser.execute_script("arguments[0].scrollIntoView(true);", button_add_build_step)
-    button_add_build_step.click()
-
-    wait.until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "jenkins-dropdown__item")))
-    browser.find_element(By.XPATH, "//button[normalize-space()='Execute shell']").click()
-
-    ActionChains(browser)\
-        .move_to_element(browser.find_element(By.XPATH, "//div[contains(@class, 'cm-s-default')]"))\
-        .click().send_keys("sleep 60").perform()
-
-    browser.find_element(By.NAME, "Submit").click()
-    wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//*[text()='Permalinks']")))
-    browser.find_element(By.CLASS_NAME, "app-jenkins-logo").click()
