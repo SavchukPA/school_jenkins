f9f672774c621ecca39781dc264b8e658c6ca9da
Petr P <none@none.none>
2026-05-10 23:17:59 +0800

    update branch

--- tests/test_manage_page.py
+++ tests/test_manage_page.py
@@ -1,3 +1,4 @@
+import selenium.webdriver.chrome.webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
@@ -5,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
 
-def create_freestyle_project_with_timer(browser, name_project: str):
+def create_freestyle_project_with_timer(browser, name_project: str, command_shell: str):
     wait = WebDriverWait(browser, 5)
 
     wait.until(expected_conditions.element_to_be_clickable((By.XPATH, "//a[@href='/view/all/newJob']"))).click()
@@ -22,12 +23,11 @@ def create_freestyle_project_with_timer(browser, name_project: str):
 
     ActionChains(browser)\
         .move_to_element(browser.find_element(By.XPATH, "//div[contains(@class, 'cm-s-default')]"))\
-        .click().send_keys("sleep 60").perform()
+        .click().send_keys(command_shell).perform()
 
     browser.find_element(By.NAME, "Submit").click()
     wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "//*[text()='Permalinks']")))
     browser.find_element(By.CLASS_NAME, "app-jenkins-logo").click()
-
 
 
 def test_verify_navigation_to_manage_page(browser):
@@ -72,7 +72,7 @@ def test_build_queue_visibility(browser):
     item_name = "job"
     count_jobs = range(1, 4)
     for i in count_jobs:
-        create_freestyle_project_with_timer(browser, item_name + str(i))
+        create_freestyle_project_with_timer(browser, item_name + str(i), "sleep 60")
 
     for _ in range(2):
         for i in count_jobs:
@@ -83,3 +83,22 @@ def test_build_queue_visibility(browser):
     target_item = [f"{item_name}{i}" for i in count_jobs]
 
     assert all(item in list_items for item in target_item)
+
+
+def test_build_history_display_status(browser: selenium.webdriver.chrome.webdriver.WebDriver):
+    name_jobs = ['job4', 'job5', 'job6']
+
+    for name in name_jobs:
+        create_freestyle_project_with_timer(browser, name, "echo $EXECUTOR_NUMBER\npwd\nls -lsa /")
+        browser.find_element(By.XPATH, f"//tr/td[7]//a[@tooltip='Schedule a Build for {name}']").click()
+
+    browser.find_element(By.XPATH, "//a[@href='/view/all/builds']").click()
+    job_color_status = browser.find_elements(By.XPATH, "//tbody/tr/td[contains(@class, 'jenkins-table__icon')]/div")[0].get_attribute('innerHTML')
+    name_job = browser.find_element(By.XPATH, "//tbody/tr/td/a[@class='jenkins-table__link model-link']").get_attribute("href")
+    timer_job = browser.find_elements(By.XPATH, "//tbody/tr/td[@data]")
+    status_job = browser.find_elements(By.XPATH, "//tbody/tr/td[4]")
+
+    assert 'id="blue"' in job_color_status or 'id="red"' in job_color_status, "нет джобы с успешным статусом выполнения"
+    assert any(job in name_job for job in name_jobs), "ни одна тестовая джоба не была запущена на выполнение"
+    assert any("sec" in timer.text for timer in timer_job), "ни у одной джобы нет прошедшего времени выполнения"
+    assert any("stable" in status.text for status in status_job), "нет джобы с успешным статусом выполнения"
